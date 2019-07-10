070fd1885e6ceb4adb24d800f171d138e9721698
Chubbard022 <chubbard022@gmail.com>
2019-07-10 08:53:01 -0700

    adding all pictures and adding main content

--- portfolio/src/TopBar.js
+++ portfolio/src/TopBar.js
@@ -11,6 +11,17 @@ class TopBar extends Component{
         return(
             <div className="topbar">
                 <h1>TopBar</h1>
+                <img src="../images/background.jpeg" alt="photo" />
+                <h1>Curtis Hubbard</h1>
+                <h4>Full Stack Web Developer From Oregon</h4>
+                <p>
+                    My name is Curtis Hubbard, I am a 22 year old full stack 
+                    developer from Eugene Oregon. I have been programming for four years.
+                    I started to program while attending College at Lane Community college and Oregon 
+                    State University for three years. After attending college for three years, I attended 
+                    Lambda School's code bootcamp for nine months.
+                </p>
+                <button class ="button">Link To Resume</button>
             </div>
         )
     }
