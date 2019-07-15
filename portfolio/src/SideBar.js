6123edb6ec7f28ee30fd1b45e5cacf80f5f67dec
Chubbard022 <chubbard022@gmail.com>
2019-07-15 08:04:14 -0700

    spacing for sidebar

--- portfolio/src/SideBar.js
+++ portfolio/src/SideBar.js
@@ -14,20 +14,30 @@ class SideBar extends Component{
                 <div className="side">
                     <h3>Articles</h3>
                     <img className="articlePhoto" src={article1} alt="missing puzzle piece"/>
-                    <a href="https://medium.com/@chubbard02/struggles-with-imposter-syndrome-2512beb72cd2">Imposter Syndrome<i class="far fa-newspaper"></i></a>
+                    <br/>
+                    <a href="https://medium.com/@chubbard02/struggles-with-imposter-syndrome-2512beb72cd2" style={{color:"#919191",textDecoration:"none"}}>Imposter Syndrome<i style={{paddingLeft:"5px"}}class="far fa-newspaper"></i></a>
+                    <br/>
+                    <br/>
+                    <br/>
                     <br/>
                     <img className="articlePhoto" src={article2} alt="lightbulb inside cloud"/>
-                    <a href="https://medium.com/@chubbard02/university-vs-bootcamp-learning-ac0f27d45ed">University Vs. Bootcamp<i class="far fa-newspaper"></i></a>
-                </div>
-                <div className="side">
+                    <a href="https://medium.com/@chubbard02/university-vs-bootcamp-learning-ac0f27d45ed" style={{color:"#919191",textDecoration:"none"}}>University Vs. Bootcamp<i style={{paddingLeft:"5px"}} class="far fa-newspaper"></i></a>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
                     <h3>Get In Touch</h3>
                     <p className="sideParagraph">
                         if you would like to get in touch with me. 
                         please leave me an email or contact me through 
                         my social media
                     </p>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
                     <a className="email" >
-                    <i class="fas fa-envelope"></i>
+                    <i class="fas fa-envelope bigger"></i>
                         chubbard02@gmial.com
                     </a>
                     <br/>
